391ea968b0e358629571db03dec4413dfc04ad5c
Captain <captain.onboard@web.de>
2020-06-02 15:34:35 +0200

    networkbrowser add python3 module init

--- networkbrowser/src/lib/main.c
+++ networkbrowser/src/lib/main.c
@@ -26,6 +26,7 @@
 #include "nbtscan.h"
 #include "smbinfo.h"
 #include "showmount.h"
+#include "Python.h"
 
 static PyObject *error;
 
@@ -185,8 +186,26 @@ static PyMethodDef netscanmethods[] = {
 	{NULL, NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
+	static struct PyModuleDef moduledef = {
+		PyModuleDef_HEAD_INIT,
+		"netscan",					/* m_name */
+		"Module for netscan",		/* m_doc */
+		-1,									/* m_size */
+		netscanmethods,			/* m_methods */
+		NULL,								/* m_reload */
+		NULL,								/* m_traverse */
+		NULL,								/* m_clear */
+		NULL,								/* m_free */
+	};
+#endif
+
 void initnetscan(void)
 {
+#if PY_MAJOR_VERSION >= 3
+	PyModule_Create(&moduledef);
+#else
 	Py_InitModule("netscan", netscanmethods);
+#endif
 }
 
